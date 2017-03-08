643c404f6145e0514263c5224783ba74f1216abf
amngupta <amangupta96@outlook.com>
2017-03-07 23:32:48 -0800

    Promised the oracle query

--- index.js
+++ index.js
@@ -4,48 +4,35 @@ var oracledb = require('oracledb');
 // ssh -l t0d1b -L localhost:1522:dbhost.ugrad.cs.ubc.ca:1522 remote.ugrad.cs.ubc.ca
 
 // Get a non-pooled connection
-oracledb.getConnection(
-  {
-    user          : "ora_t0d1b",
-    password      : "a70746169",
-    connectString : "localhost:1522/ug"
-  },
-  function(err, connection)
-  {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    connection.execute(
+var connection;
+
+// Get a non-pooled connection
+oracledb.getConnection({
+  user: "ora_t0d1b",
+  password: "a70746169",
+  connectString: "localhost:1522/ug"
+})
+  .then(conn => {
+    connection = conn;
+    return conn.execute(
       // The statement to execute
-      "select pub_name from publishers where state in ('CA','IN','MD')",
-
-      // The "bind value" 180 for the "bind variable" :id
-      // [180],
-
-      // Optional execute options argument, such as the query result format
-      // or whether to get extra metadata
-      // { outFormat: oracledb.OBJECT, extendedMetaData: true },
-
-      // The callback function handles the SQL execution results
-      function(err, result)
-      {
-        if (err) {
-          console.error(err.message);
-          doRelease(connection);
-          return;
-        }
-        console.log(result.metaData); // [ { name: 'DEPARTMENT_ID' }, { name: 'DEPARTMENT_NAME' } ]
-        console.log(result.rows);     // [ [ 180, 'Construction' ] ]
-        doRelease(connection);
-      });
-  });
+      "select pub_name from publishers where state in ('CA','IN','MD')"
+    )
+  })
+  .then(result => {
+    console.log(result.metaData); // [ { name: 'DEPARTMENT_ID' }, { name: 'DEPARTMENT_NAME' } ]
+    console.log(result.rows);     // [ [ 180, 'Construction' ] ]
+    close(connection);
+  })
+  .catch(err => {
+    console.log(err);
+    close(connection);
+  })
 
 // Note: connections should always be released when not needed
-function doRelease(connection)
-{
+var doRelease = (connection) => {
   connection.close(
-    function(err) {
+    function (err) {
       if (err) {
         console.error(err.message);
       }
