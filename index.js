d109919d5b9ba996ce31ea2163261699e8c92925
amngupta <amangupta96@outlook.com>
2017-03-14 20:11:34 -0700

    Script to set / reset DB

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var oracledb = require('oracledb');
+import * as fs from 'fs';
+import dbConfig from './config';
 
 // export LD_LIBRARY_PATH=/opt/oracle/instantclient:$LD_LIBRARY_PATH
 // ssh -l t0d1b -L localhost:1522:dbhost.ugrad.cs.ubc.ca:1522 remote.ugrad.cs.ubc.ca
@@ -6,32 +8,47 @@ var oracledb = require('oracledb');
 // Get a non-pooled connection
 var connection;
 
+let initString = () => {
+  return new Promise((fulfill, reject) => {
+    fs.readFile("Project_DDL.txt", 'utf-8', (err, data) => {
+      if (!err) {
+        data.toString('ascii', 0, data.length);
+        let commands = data.split(";");
+        fulfill(commands);
+      }
+    });
+  });
+}
+
 // Get a non-pooled connection
 oracledb.getConnection({
-  user: "ora_t0d1b",
-  password: "a70746169",
-  connectString: "localhost:1522/ug"
+  user: dbConfig.user,
+  password: dbConfig.password,
+  connectString: dbConfig.connecturl
 })
   .then(conn => {
     connection = conn;
-    return conn.execute(
-      // The statement to execute
-      "select pub_name from publishers where state in ('CA','IN','MD')"
-    )
+    return initString()
+      .then(commands => {
+        commands.forEach(command => {
+          return conn.execute(command)
+        })
+        return conn.execute("SELECT table_name FROM user_tables");
+      });
   })
   .then(result => {
     console.log(result.metaData); // [ { name: 'DEPARTMENT_ID' }, { name: 'DEPARTMENT_NAME' } ]
     console.log(result.rows);     // [ [ 180, 'Construction' ] ]
-    close(connection);
+    doRelease(connection);
   })
   .catch(err => {
     console.log(err);
-    close(connection);
+    doRelease(connection);
   })
 
 // Note: connections should always be released when not needed
-var doRelease = (connection) => {
-  connection.close(
+var doRelease = (conn) => {
+  conn.close(
     function (err) {
       if (err) {
         console.error(err.message);
