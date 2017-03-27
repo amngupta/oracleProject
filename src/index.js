17d5c516940eae51a81d1593c0ed8be7a33e3e87
amngupta <amangupta96@outlook.com>
2017-03-27 13:28:04 -0700

    Added new bootstrap theme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import ReactDOM from 'react-dom';
-// import { Router } from 'react-router';
-// import App from './App';
 import Routes from './routes';
 import './index.css';
-// import { browserHistory } from 'react-router';
-
-
 
 ReactDOM.render(
   Routes,
