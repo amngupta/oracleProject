f83edb221528f7e4ddb502049b87a3620f1e5327
Nixon Kwok <nixonkwok@gmail.com>
2017-03-30 00:09:57 -0700

    Implement viewProjects update and delete

--- src/components/president/viewProjects.jsx
+++ src/components/president/viewProjects.jsx
@@ -21,6 +21,8 @@ export default class ViewProjects extends Component {
         this.open = this.open.bind(this);
         this.doQuery = this.doQuery.bind(this);
         this.onClickRow = this.onClickRow.bind(this);
+        this.onClickUpdate = this.onClickUpdate.bind(this);
+        this.onClickDelete = this.onClickDelete.bind(this);
     }
 
     componentWillMount() {
@@ -82,7 +84,7 @@ export default class ViewProjects extends Component {
     }
 
     doQuery(e) {
-        e.preventDefault();
+        if (e) { e.preventDefault(); }
         let id = this.pid.value || null;
         let name = this.pname.value || null;
         let aggr = this.waggr.value || null;
@@ -146,6 +148,70 @@ export default class ViewProjects extends Component {
             });
     }
 
+    onClickUpdate() {
+        let pid = this.peditId.value || null;
+        if (!pid) { return (console.error("PID is null...")); }
+        let name = this.peditName.value || null;
+        let budg = this.peditBudget.value || null;
+        let desc = this.pdescription.value || null;
+
+        let update = "";
+        if (name && (name !== this.state.modalData.NAME)) {
+            update = "p.name='" + name + "'";
+        }
+        if (budg && (budg !== this.state.modalData.BUDGET)) {
+            let set = "p.budget='" + budg + "'";
+            update = update ? update + ", " + set : set;
+        }
+        if (desc && (desc !== this.state.modalData.DESCRIPTION)) {
+            let set = "p.description='" + desc + "'";
+            update = update ? update + ", " + set : set;
+        }
+        if (!update) { return (alert("No changes to update!")); }
+
+        let query = "UPDATE projectbudget p SET " + update + " WHERE p.pid=" + pid;
+        let self = this;
+        let options = {
+            uri: 'http://localhost:9000/query/' + encodeURI(query),
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true
+        };
+        request(options)
+            .then(function (body) {
+                console.log(body);
+                self.doQuery();
+                self.close();
+            })
+            .catch(function (err) {
+                console.error(err);
+            });
+    }
+
+    onClickDelete() {
+        let id = this.peditId.value || null;
+        if (!id) { return (console.error("ID is null...")); }
+
+        let query = "DELETE FROM projectbudget p WHERE p.pid=" + id;
+        let self = this;
+        let options = {
+            uri: 'http://localhost:9000/query/' + encodeURI(query),
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true
+        };
+        request(options)
+            .then(function (body) {
+                console.log(body);
+                self.doQuery();
+                self.close();
+            })
+            .catch(function (err) {
+                console.error(err);
+            });
+    }
 
     render() {
         const button = (<Button bsStyle="success">Add New Project </Button>)
@@ -206,19 +272,27 @@ export default class ViewProjects extends Component {
                                 <div className="form-group col-sm-6">
                                     <label className="control-label text-semibold col-sm-4 col-md-3">Id Number</label>
                                     <div className="col-sm-8 col-md-9">
-                                        <input type='number' disabled name='name' ref={ref => this.peditId = ref} placeholder='ID Number' value={this.state.modalData.PID} className="form-control" />
+                                        <input type='number' disabled name='name' ref={ref => this.peditId = ref} placeholder='ID Number' defaultValue={this.state.modalData.PID} className="form-control" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="form-group col-sm-6">
+                                    <label className="control-label text-semibold col-sm-4 col-md-3">Budget</label>
+                                    <div className="col-sm-8 col-md-9">
+                                        <input type='number' name='name' ref={ref => this.peditBudget = ref} placeholder='1234' defaultValue={this.state.modalData.BUDGET} className="form-control" />
                                     </div>
                                 </div>
                                 <div className="form-group col-sm-6">
                                     <label className="control-label text-semibold col-sm-4 col-md-3">Project Name</label>
                                     <div className="col-sm-8 col-md-9">
-                                        <input type='text' name='name' ref={ref => this.peditName = ref} placeholder='Project Name' value={this.state.modalData.NAME} className="form-control" />
+                                        <input type='text' name='name' ref={ref => this.peditName = ref} placeholder='Project Name' defaultValue={this.state.modalData.NAME} className="form-control" />
                                     </div>
                                 </div>
                                 <div className="form-group col-sm-12">
                                     <label className="control-label text-semibold col-sm-4 col-md-3">Project Description</label>
                                     <div className="col-sm-8 col-md-9">
-                                        <textarea ref={ref => this.pdescription = ref} className="form-control" placeholder="Project Description" value={this.state.modalData.DESCRIPTION} ></textarea>
+                                        <textarea ref={ref => this.pdescription = ref} className="form-control" placeholder="Project Description" defaultValue={this.state.modalData.DESCRIPTION} maxLength={1000} ></textarea>
                                     </div>
                                 </div>
                             </div>
@@ -237,10 +311,10 @@ export default class ViewProjects extends Component {
                         {modalBody}
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button bsStyle="success">
+                        <Button bsStyle="success" onClick={this.onClickUpdate}>
                             Update
                             </Button>
-                        <Button bsStyle="danger">
+                        <Button bsStyle="danger" onClick={this.onClickDelete}>
                             Delete
                             </Button>
                     </Modal.Footer>
